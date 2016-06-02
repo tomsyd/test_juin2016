3e00388f54940303f5487af02538c664abda4c28
Donghua Jiang <djiang@amadeus.com>
2016-06-02 11:43:20 +0200

    test2.py only working for small sized booking file, but it can display with the airport name

--- README.md
+++ README.md
@@ -3,5 +3,9 @@
 > python test1.py fileName
 
 bookings.csv: 10000011 lines
+
 searches.csv: 20390199 lines
 
+> python test2.py smallBookingFile
+
+the current version is only working for small size booking file
---
+++ test2.py
@@ -0,0 +1,25 @@
+import sys,os,pandas,csv
+from GeoBases import GeoBase
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print 'Please provide a booking file.'
+        sys.exit(0)
+    aFile = sys.argv[1]
+    aData = None
+    if not os.path.exists(aFile):
+        print 'Please check if the booking file', aFile, 'does exist'
+        sys.exit(0)
+    else:
+        # extract data from file
+        aData = pandas.read_csv(aFile,sep='^',low_memory=False)
+        grouped = aData.groupby(aData['arr_port']).sum()
+        sgrouped = grouped.sort_values(by='pax',ascending=False)
+        geo = GeoBase(data='ori_por', verbose=False)
+        for i in range(10):
+            try:
+                item = sgrouped.ix[i]
+                print geo.get(item.name.strip(),'name'),item.pax
+            except:
+                print 'no more airports...'
+                sys.exit(0)
